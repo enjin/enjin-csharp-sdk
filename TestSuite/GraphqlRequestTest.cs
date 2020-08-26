5cc3a3597158958db68a4ed0f89ce901b73edb0c
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-08-26 13:50:24 -0700

    Added unit tests for GraphqlRequest

---
+++ TestSuite/GraphqlRequestTest.cs
@@ -0,0 +1,63 @@
+﻿using Enjin.SDK.Graphql;
+using NUnit.Framework;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class GraphqlRequestTest
+    {
+        [Test]
+        [TestCase(1)]
+        [TestCase(1.0)]
+        [TestCase(1.0f)]
+        [TestCase(1L)]
+        [TestCase('1')]
+        [TestCase("1")]
+        [TestCase(true)]
+        public void SetVariable_MultipleTypes_RequestContainsVariable(object v)
+        {
+            // Arrange
+            const string k = "variable";
+            var request = new TestableGraphqlRequest();
+
+            // Act
+            request.SetVariable(k, v);
+
+            // Assert
+            Assert.AreSame(v, request.Variables[k]);
+        }
+
+        [Test]
+        public void IsSet_ReturnTrue()
+        {
+            // Arrange
+            const string k = "variable";
+            const int v = 0;
+            var request = new TestableGraphqlRequest();
+
+            // Act
+            request.SetVariable(k, v);
+
+            // Assert
+            Assert.IsTrue(request.IsSet(k));
+        }
+
+        [Test]
+        public void IsSet_ReturnFalse()
+        {
+            // Arrange
+            const string k = "variable";
+            var request = new TestableGraphqlRequest();
+
+            // Assert
+            Assert.IsFalse(request.IsSet(k));
+        }
+
+        private class TestableGraphqlRequest : GraphqlRequest<TestableGraphqlRequest>
+        {
+            public TestableGraphqlRequest() : base("")
+            {
+            }
+        }
+    }
+}
