f079b44bf29b3525eb2208f7d01440231aab9e13
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-09-04 15:51:36 -0700

    Added documentation for events

--- EnjinCSharpSDK/Events/EventFilter.cs
+++ EnjinCSharpSDK/Events/EventFilter.cs
@@ -4,13 +4,32 @@ using JetBrains.Annotations;
 
 namespace Enjin.SDK.Events
 {
+    /// <summary>
+    /// Specifies the events to be allowed or ignored for a listener.
+    /// </summary>
+    /// <seealso cref="IEventListener"/>
+    /// <seealso cref="IEventService"/>
     [PublicAPI]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
     public class EventFilter : Attribute
     {
+        /// <summary>
+        /// Represents if the events are to be allowed (<c>true</c>) or ignored (<c>false</c>).
+        /// </summary>
+        /// <value>The state.</value>
         public bool Allowed { get; }
+        
+        /// <summary>
+        /// Represents the events to be filtered and allowed or ignored based on <see cref="Allowed"/>.
+        /// </summary>
+        /// <value></value>
         public EventType[] Types { get; }
 
+        /// <summary>
+        /// Constructs the attribute.
+        /// </summary>
+        /// <param name="allowed">If the events are allowed.</param>
+        /// <param name="types">The events being filtered.</param>
         public EventFilter(bool allowed, params EventType[] types)
         {
             Allowed = allowed;
--- EnjinCSharpSDK/Events/Services/PusherEventService.cs
+++ EnjinCSharpSDK/Events/Services/PusherEventService.cs
@@ -11,20 +11,34 @@ using PusherOptions = PusherClient.PusherOptions;
 
 namespace Enjin.SDK.Events
 {
+    /// <summary>
+    /// Implementation of <see cref="IEventService"/> utilizing Pusher channel events.
+    /// </summary>
+    /// <seealso cref="IEventListener"/>
     [PublicAPI]
     public sealed class PusherEventService : IEventService
     {
+        /// <summary>
+        /// Represents the platform details this service is utilizing.
+        /// </summary>
+        /// <value>The platform details.</value>
         public Platform Platform { get; private set; }
+        
         internal List<EventListenerRegistration> RegisteredListeners { get; } = new List<EventListenerRegistration>();
         private readonly Dictionary<string, Channel> _subscribed = new Dictionary<string, Channel>();
         private Pusher? _pusher;
         private PusherEventListener? _listener;
 
+        /// <summary>
+        /// Sole constructor. See <see cref="Start()"/> to start the service.
+        /// </summary>
+        /// <param name="platform">The platform details.</param>
         public PusherEventService(Platform platform)
         {
             Platform = platform;
         }
 
+        /// <inheritdoc/>
         public void Start()
         {
             Shutdown();
@@ -49,33 +63,40 @@ namespace Enjin.SDK.Events
             _pusher.ConnectAsync();
         }
 
+        /// <inheritdoc/>
         public void Start(Platform platform)
         {
             Platform = platform;
             Start();
         }
 
+        /// <inheritdoc/>
         public void Shutdown() => _pusher?.DisconnectAsync();
 
+        /// <inheritdoc/>
         public bool IsConnected() => _pusher?.State == ConnectionState.Connected;
 
+        /// <inheritdoc/>
         public EventListenerRegistration RegisterListener(IEventListener listener)
         {
             return Register(ConfigureListener(listener));
         }
 
+        /// <inheritdoc/>
         public EventListenerRegistration RegisterListenerWithMatcher(IEventListener listener,
             Func<EventType, bool> matcher)
         {
             return Register(ConfigureListener(listener).WithMatcher(matcher));
         }
 
+        /// <inheritdoc/>
         public EventListenerRegistration RegisterListenerIncludingTypes(IEventListener listener,
             params EventType[] events)
         {
             return Register(ConfigureListener(listener).WithAllowedEvents(events));
         }
 
+        /// <inheritdoc/>
         public EventListenerRegistration RegisterListenerExcludingTypes(IEventListener listener,
             params EventType[] events)
         {
@@ -92,6 +113,7 @@ namespace Enjin.SDK.Events
             return configuration.Create();
         }
 
+        /// <inheritdoc/>
         public void UnregisterListener(IEventListener listener)
         {
             EventListenerRegistration reg =
@@ -100,61 +122,73 @@ namespace Enjin.SDK.Events
                 RegisteredListeners.Remove(reg);
         }
 
+        /// <inheritdoc/>
         public void SubscribeToApp(int app)
         {
             Subscribe(new PusherAppChannel(Platform, app).Channel());
         }
 
+        /// <inheritdoc/>
         public void UnsubscribeToApp(int app)
         {
             Unsubscribe(new PusherAppChannel(Platform, app).Channel());
         }
 
+        /// <inheritdoc/>
         public bool IsSubscribedToApp(int app)
         {
             return _subscribed.ContainsKey(new PusherAppChannel(Platform, app).Channel());
         }
 
+        /// <inheritdoc/>
         public void SubscribeToPlayer(int app, string player)
         {
             Subscribe(new PusherPlayerChannel(Platform, app, player).Channel());
         }
 
+        /// <inheritdoc/>
         public void UnsubscribeToPlayer(int app, string player)
         {
             Unsubscribe(new PusherPlayerChannel(Platform, app, player).Channel());
         }
 
+        /// <inheritdoc/>
         public bool IsSubscribedToPlayer(int app, string player)
         {
             return _subscribed.ContainsKey(new PusherPlayerChannel(Platform, app, player).Channel());
         }
 
+        /// <inheritdoc/>
         public void SubscribeToToken(string token)
         {
             Subscribe(new PusherTokenChannel(Platform, token).Channel());
         }
 
+        /// <inheritdoc/>
         public void UnsubscribeToToken(string token)
         {
             Unsubscribe(new PusherTokenChannel(Platform, token).Channel());
         }
 
+        /// <inheritdoc/>
         public bool IsSubscribedToToken(string token)
         {
             return _subscribed.ContainsKey(new PusherTokenChannel(Platform, token).Channel());
         }
 
+        /// <inheritdoc/>
         public void SubscribeToWallet(string wallet)
         {
             Subscribe(new PusherWalletChannel(Platform, wallet).Channel());
         }
 
+        /// <inheritdoc/>
         public void UnsubscribeToWallet(string wallet)
         {
             Unsubscribe(new PusherWalletChannel(Platform, wallet).Channel());
         }
 
+        /// <inheritdoc/>
         public bool IsSubscribedToWallet(string wallet)
         {
             return _subscribed.ContainsKey(new PusherWalletChannel(Platform, wallet).Channel());
