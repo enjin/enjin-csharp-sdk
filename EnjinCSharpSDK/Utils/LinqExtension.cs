c6a07b8a26f987a663f2256f27685a0bc3b27aee
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-09-07 11:17:16 -0700

    Added documentation utilities

--- EnjinCSharpSDK/Utils/LinqExtension.cs
+++ EnjinCSharpSDK/Utils/LinqExtension.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 namespace Enjin.SDK.Utils
 {
+    /// <summary>
+    /// Class with extension methods for LINQ expressions.
+    /// </summary>
     public static class LinqExtension
     {
+        /// <summary>
+        /// Performs an action on each element.
+        /// </summary>
+        /// <param name="elements">An <see cref="IEnumerable{T}"/> to perform an action in.</param>
+        /// <param name="action">An action to perform.</param>
+        /// <typeparam name="T">The type of the elements of the source.</typeparam>
         public static void Do<T>(this IEnumerable<T> elements, Action<T> action)
         {
             foreach (var e in elements)
--- EnjinCSharpSDK/Utils/TextFormatting.cs
+++ EnjinCSharpSDK/Utils/TextFormatting.cs
@@ -1,8 +1,18 @@
 namespace Enjin.SDK.Utils
 {
+    /// <summary>
+    /// Utility class for text formatting.
+    /// </summary>
     public static class TextFormatting
     {
+        /// <summary>
+        /// An empty string.
+        /// </summary>
         public const string EmptyString = "";
+        
+        /// <summary>
+        /// A delimiter for comma-space separated sequences.
+        /// </summary>
         public const string CommaSpaceDelimiter = ", ";
     }
 }
