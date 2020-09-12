8e3959745d1b374d1facdc1ee64399a433268850
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-09-11 19:02:41 -0700

    Added unit tests for GraphqlTemplate

---
+++ TestSuite/GraphqlTemplateTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Enjin.SDK.Graphql;
+using NUnit.Framework;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class GraphqlTemplateTest
+    {
+        [Test]
+        [TestCase("enjin.graphql.Template")]
+        public void ReadNamespace_ContentsDoesHaveNamespace_ReturnsTheNamespace(string expected)
+        {
+            // Arrange
+            var contents = new List<string>
+            {
+                "dummy line",
+                $"{GraphqlTemplate.NAMESPACE_KEY} {expected}",
+                "dummy line",
+            };
+
+            // Act
+            var actual = GraphqlTemplate.ReadNamespace(contents);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void ReadNamespace_ContentsDoesNotHaveNamespace_ReturnsNull()
+        {
+            // Arrange
+            var contents = new List<string>
+            {
+                "dummy line",
+                "dummy line",
+                "dummy line",
+            };
+
+            // Act
+            var actual = GraphqlTemplate.ReadNamespace(contents);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+    }
+}
