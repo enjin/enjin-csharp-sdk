f83db123306eaa93d530e4b5c3980dd393a6983d
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-02 16:39:40 -0700

    - Added DummyObject class to TestSuite.Utils - Added unit tests for GraphqlData - Added unit tests for GraphqlResponse

---
+++ TestSuite/Unit/Graphql/GraphqlDataTest.cs
@@ -0,0 +1,71 @@
+﻿using Enjin.SDK.Graphql;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using TestSuite.Utils;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class GraphqlDataTest
+    {
+        private static readonly DummyObject DEFAULT_OBJECT = new DummyObject(1);
+
+        [Test]
+        public void Constructor_ObjectToken_DoesNotThrowException()
+        {
+            // Arrange
+            var result = JToken.Parse(@"{}");
+            
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlData<object>(result));
+        }
+        
+        [Test]
+        public void Constructor_NonObjectToken_DoesNotThrowException()
+        {
+            // Arrange
+            var result = JToken.Parse(@"'primitive'");
+            
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlData<object>(result));
+        }
+        
+        [Test]
+        public void Constructor_PaginatedToken_DoesNotThrowException()
+        {
+            // Arrange
+            var result = JToken.Parse(@"{'items': [], 'cursor': {}}");
+            
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlData<object>(result));
+        }
+        
+        [Test]
+        public void Result_GetsValue()
+        {
+            // Arrange
+            var expected = DEFAULT_OBJECT;
+            var result = JObject.FromObject(expected);
+
+            // Act
+            var actual = new GraphqlData<DummyObject>(result).Result;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToString_ReturnsStringWithResult()
+        {
+            // Arrange
+            var expected = $"Result: {DEFAULT_OBJECT}";
+            var result = JObject.FromObject(DEFAULT_OBJECT);
+
+            // Act
+            var actual = new GraphqlData<DummyObject>(result).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TestSuite/Unit/Graphql/GraphqlResponseTest.cs
@@ -0,0 +1,151 @@
+﻿using Enjin.SDK.Graphql;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class GraphqlResponseTest
+    {
+        private static readonly GraphqlData<object> DEFAULT_DATA =
+            new GraphqlData<object>(JObject.FromObject(new object()));
+
+        private static readonly JToken DEFAULT_ERRORS = new JArray();
+
+        [Test]
+        public void Constructor_NonNullDataNonNullErrors_DoesNotThrowException()
+        {
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlResponse<object>(DEFAULT_DATA, DEFAULT_ERRORS));
+        }
+
+        [Test]
+        public void Constructor_NonNullDataNullErrors_DoesNotThrowException()
+        {
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlResponse<object>(DEFAULT_DATA, null));
+        }
+
+        [Test]
+        public void Constructor_NullDataNonNullErrors_DoesNotThrowException()
+        {
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlResponse<object>(null, DEFAULT_ERRORS));
+        }
+
+        [Test]
+        public void Constructor_NullDataNullErrors_DoesNotThrowException()
+        {
+            // Assert
+            Assert.DoesNotThrow(() => new GraphqlResponse<object>(null, null));
+        }
+
+        [Test]
+        public void Result_NonNullResultInData_GetsValue()
+        {
+            // Arrange
+            var expected = DEFAULT_DATA.Result;
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, DEFAULT_ERRORS);
+
+            // Act
+            var actual = response.Result;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Result_NullData_GetsValue()
+        {
+            // Arrange
+            var response = new GraphqlResponse<object>(null, DEFAULT_ERRORS);
+
+            // Act
+            var actual = response.Result;
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void Errors_NonNullErrors_GetsValue()
+        {
+            // Arrange
+            var expected = DEFAULT_ERRORS;
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, expected);
+
+            // Act
+            var actual = response.Errors;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Errors_NullErrors_GetsValue()
+        {
+            // Arrange
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, null);
+
+            // Act
+            var actual = response.Errors;
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void HasErrors_ErrorsArePresent_ReturnsTrue()
+        {
+            // Arrange
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, DEFAULT_ERRORS);
+
+            // Act
+            var actual = response.HasErrors();
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void HasErrors_ErrorsAreNotPresent_ReturnsFalse()
+        {
+            // Arrange
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, null);
+
+            // Act
+            var actual = response.HasErrors();
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void ToString_DoesNotHaveErrors_ReturnsStringContainingResult()
+        {
+            // Arrange
+            var expected = $"Result: {DEFAULT_DATA.Result}";
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, null);
+
+            // Act
+            var actual = response.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToString_DoesHaveErrors_ReturnsStringContainingErrors()
+        {
+            // Arrange
+            var expected = $"Errors: {DEFAULT_ERRORS}";
+            var response = new GraphqlResponse<object>(DEFAULT_DATA, DEFAULT_ERRORS);
+
+            // Act
+            var actual = response.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
