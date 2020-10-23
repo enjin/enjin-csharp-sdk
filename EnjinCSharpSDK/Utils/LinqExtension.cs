e0c2c42c76eb7785507fd3b139b0409a630e6bac
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-23 13:57:55 -0700

    - Added Moq as a dependency to TestSuite - Added unit tests for LinqExtension

--- EnjinCSharpSDK/Utils/LinqExtension.cs
+++ EnjinCSharpSDK/Utils/LinqExtension.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("TestSuite")]
 namespace Enjin.SDK.Utils
 {
     /// <summary>
