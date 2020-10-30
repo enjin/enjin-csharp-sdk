64d4391a96ea46b342812074898096be1d578366
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-30 15:06:46 -0700

    - Corrected test name in PusherEventListenerTest - Corrected verify for unit test in PusherEventListenerTest - Added test case to PusherEventListenerTest for when registered listener excludes event type

--- TestSuite/Unit/Events/Listeners/PusherEventListenerTest.cs
+++ TestSuite/Unit/Events/Listeners/PusherEventListenerTest.cs
@@ -49,7 +49,7 @@ namespace TestSuite.Listeners
         [TestCase("EnjinCloud\\Events\\PlayerLinked")]
         [TestCase("EnjinCloud\\Events\\TokenTransferred")]
         [TestCase("EnjinCloud\\Events\\TransactionExecuted")]
-        public void OnEvent_(string eventName)
+        public void OnEvent_ValidEventForRegisteredListener_DoesCallRegisteredListener(string eventName)
         {
             // Arrange - Data
             const string channelName = "";
@@ -67,7 +67,29 @@ namespace TestSuite.Listeners
 
             // Verify
             Mock.Get(mockEventListener)
-                .Verify(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()), Times.AtMostOnce);
+                .Verify(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()), Times.Once);
+        }
+        
+        [TestCase("EnjinCloud\\Events\\AppCreated")]
+        public void OnEvent_InvalidEventForRegisteredListener_DoesNotCallRegisteredListener(string eventName)
+        {
+            // Arrange - Data
+            const string channelName = "";
+            const string data = "";
+            var fakePusherEvent = CreateFakePusherEvent(eventName, channelName, data, "");
+            var mockEventListener = Mock.Of<IEventListener>();
+            FakeEventService.RegisterListenerExcludingTypes(mockEventListener, EventType.APP_CREATED);
+            
+            // Arrange - Expectations
+            Mock.Get(mockEventListener)
+                .Setup(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()));
+
+            // Act
+            ClassUnderTest.OnEvent(fakePusherEvent);
+
+            // Verify
+            Mock.Get(mockEventListener)
+                .Verify(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()), Times.Never);
         }
 
         private static PusherEvent CreateFakePusherEvent(string eventName, string channelName, string data, string raw)
