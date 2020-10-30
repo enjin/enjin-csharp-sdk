d49c8f5dc8d7dd10fec675b892658b17551b4311
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-30 13:17:41 -0700

    Added unit tests for PusherEventListener

---
+++ TestSuite/Unit/Events/Listeners/PusherEventListenerTest.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using Enjin.SDK.Events;
+using Enjin.SDK.Models;
+using Moq;
+using NUnit.Framework;
+using PusherClient;
+using TestSuite.Utils;
+
+namespace TestSuite.Listeners
+{
+    [TestFixture]
+    public class PusherEventListenerTest
+    {
+        private PusherEventService FakeEventService { get; set; }
+        
+        private PusherEventListener ClassUnderTest { get; set; }
+        
+        [SetUp]
+        public void BeforeEach()
+        {
+            FakeEventService = new PusherEventService(PlatformUtils.CreateFakePlatform("test"));
+            ClassUnderTest = new PusherEventListener(FakeEventService);
+        }
+
+        [Test]
+        public void OnEvent_UnknownEventType_DoesNothing()
+        {
+            // Arrange - Data
+            const string eventName = "";
+            const string channelName = "";
+            const string data = "";
+            var fakePusherEvent = CreateFakePusherEvent(eventName, channelName, data, "");
+            var mockEventListener = Mock.Of<IEventListener>();
+            FakeEventService.RegisterListener(mockEventListener);
+
+            // Arrange - Expectations
+            Mock.Get(mockEventListener)
+                .Setup(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()));
+
+            // Act
+            ClassUnderTest.OnEvent(fakePusherEvent);
+
+            // Verify
+            Mock.Get(mockEventListener)
+                .Verify(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()), Times.Never);
+        }
+
+        [TestCase("EnjinCloud\\Events\\AppCreated")]
+        [TestCase("EnjinCloud\\Events\\PlayerLinked")]
+        [TestCase("EnjinCloud\\Events\\TokenTransferred")]
+        [TestCase("EnjinCloud\\Events\\TransactionExecuted")]
+        public void OnEvent_(string eventName)
+        {
+            // Arrange - Data
+            const string channelName = "";
+            const string data = "";
+            var fakePusherEvent = CreateFakePusherEvent(eventName, channelName, data, "");
+            var mockEventListener = Mock.Of<IEventListener>();
+            FakeEventService.RegisterListener(mockEventListener);
+            
+            // Arrange - Expectations
+            Mock.Get(mockEventListener)
+                .Setup(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()));
+
+            // Act
+            ClassUnderTest.OnEvent(fakePusherEvent);
+
+            // Verify
+            Mock.Get(mockEventListener)
+                .Verify(mock => mock.NotificationReceived(It.IsAny<NotificationEvent>()), Times.AtMostOnce);
+        }
+
+        private static PusherEvent CreateFakePusherEvent(string eventName, string channelName, string data, string raw)
+        {
+            return new PusherEvent(new Dictionary<string, object>
+            {
+                {"event", eventName},
+                {"channel", channelName},
+                {"data", data},
+            }, raw);
+        }
+    }
+}
