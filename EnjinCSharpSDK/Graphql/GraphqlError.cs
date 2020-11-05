7d23d8f60a6223793fa1b419d02b73ea4f171577
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-11-04 17:52:14 -0800

    - Added PaginationCursor - Added GraphqlError

---
+++ EnjinCSharpSDK/Graphql/GraphqlError.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Enjin.SDK.Graphql
+{
+    /// <summary>
+    /// Models the structure of a GraphQL response error.
+    /// </summary>
+    [PublicAPI]
+    public class GraphqlError
+    {
+        /// <summary>
+        /// Represents the error message.
+        /// </summary>
+        /// <value>The error message.</value>
+        public string? Message { get; private set; }
+        
+        /// <summary>
+        /// Represents the error code.
+        /// </summary>
+        /// <value>The error code.</value>
+        public int? Code { get; private set; }
+        
+        /// <summary>
+        /// Represents the error locations.
+        /// </summary>
+        /// <value>The error locations.</value>
+        public List<Dictionary<string, int>>? Locations { get; private set; }
+        
+        /// <summary>
+        /// Represents the error details.
+        /// </summary>
+        /// <value>The error details.</value>
+        public string? Details { get; private set; }
+    }
+}
