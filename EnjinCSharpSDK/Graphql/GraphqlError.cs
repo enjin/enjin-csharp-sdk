022e75cdc1ba9cc4b90cea2a348d52364e023fef
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-11-06 15:14:19 -0800

    Added JsonProperty attribute to properties in GraphqlError

--- EnjinCSharpSDK/Graphql/GraphqlError.cs
+++ EnjinCSharpSDK/Graphql/GraphqlError.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using JetBrains.Annotations;
+using Newtonsoft.Json;
 
 namespace Enjin.SDK.Graphql
 {
@@ -13,24 +14,28 @@ namespace Enjin.SDK.Graphql
         /// Represents the error message.
         /// </summary>
         /// <value>The error message.</value>
+        [JsonProperty("message")]
         public string? Message { get; private set; }
         
         /// <summary>
         /// Represents the error code.
         /// </summary>
         /// <value>The error code.</value>
+        [JsonProperty("code")]
         public int? Code { get; private set; }
         
         /// <summary>
         /// Represents the error locations.
         /// </summary>
         /// <value>The error locations.</value>
+        [JsonProperty("locations")]
         public List<Dictionary<string, int>>? Locations { get; private set; }
         
         /// <summary>
         /// Represents the error details.
         /// </summary>
         /// <value>The error details.</value>
+        [JsonProperty("details")]
         public string? Details { get; private set; }
     }
 }
