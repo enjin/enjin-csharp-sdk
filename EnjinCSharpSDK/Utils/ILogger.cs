83c01f1b65e9c6361a69ed3df35684e7ed855d73
BishopJohnson <bishop@enjin.io>
2021-04-21 00:51:35 -0700

    - Added ILogger interface - Added LoggerProvider class

---
+++ EnjinCSharpSDK/Utils/ILogger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Enjin.SDK.Utils
+{
+    /// <summary>
+    /// Represent the different logging severity levels.
+    /// </summary>
+    [PublicAPI]
+    public enum LogLevel
+    {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        SEVERE,
+    }
+
+    /// <summary>
+    /// Logger interface to be used in the SDK.
+    /// </summary>
+    [PublicAPI]
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs the message at the given logging level.
+        /// </summary>
+        /// <param name="level">The logging level.</param>
+        /// <param name="message">The message.</param>
+        void Log(LogLevel level, string message);
+
+        /// <summary>
+        /// Formats and logs the message and exception at the given logging level.
+        /// </summary>
+        /// <param name="level">The logging level.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="e">The exception.</param>
+        void Log(LogLevel level, string message, Exception e);
+
+        /// <summary>
+        /// Determines if the given logging level is enabled for this logger.
+        /// </summary>
+        /// <param name="level">The logging level.</param>
+        /// <returns>Whether this logger is enabled for the given logging level.</returns>
+        bool IsLoggable(LogLevel level);
+    }
+}
---
+++ TestSuite/Unit/Utils/LoggerProviderTest.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Enjin.SDK.Utils;
+using Moq;
+using NUnit.Framework;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class LoggerProviderTest
+    {
+        private const string DefaultMessage = "Test Message";
+        
+        private LoggerProvider ClassUnderTest { get; set; }
+        
+        private ILogger MockLogger { get; set; }
+
+        private static Exception CreateDefaultException()
+        {
+            return new Exception("Default Exception");
+        }
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            MockLogger = Mock.Of<ILogger>();
+            ClassUnderTest = new LoggerProvider(MockLogger);
+        }
+        
+        [Test]
+        public void Log_GivenMessage_LogsDataAtDefaultLevel()
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+            LogLevel level = ClassUnderTest.DefaultLevel;
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(true)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Log(message);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message), Times.Once);
+        }
+        
+        [Test]
+        public void Debug_GivenMessage_LogsDataAtDebugLevel()
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+            LogLevel level = ClassUnderTest.DebugLevel;
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(true)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Debug(message);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message), Times.Once);
+        }
+        
+        [Test]
+        public void Log_GivenMessageWithLoggableLevel_LogsDataAtLogLevel([Values] LogLevel level)
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(true)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Log(level, message);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message), Times.Once);
+        }
+        
+        [Test]
+        public void Log_GivenMessageWithNonLoggableLevel_DoesNotLogData([Values] LogLevel level)
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(false)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Log(level, message);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message), Times.Never);
+        }
+
+        [Test]
+        public void Log_GivenMessageAndException_LogsDataAtDefaultLevel()
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+            Exception exception = CreateDefaultException();
+            LogLevel level = ClassUnderTest.DefaultLevel;
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(true)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message, exception))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Log(message, exception);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message, exception), Times.Once);
+        }
+        
+        [Test]
+        public void Debug_GivenMessageAndException_LogsDataAtDebugLevel()
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+            Exception exception = CreateDefaultException();
+            LogLevel level = ClassUnderTest.DebugLevel;
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(true)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message, exception))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Debug(message, exception);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message, exception), Times.Once);
+        }
+        
+        [Test]
+        public void Log_GivenMessageAndExceptionWithLoggableLevel_LogsDataAtLogLevel([Values] LogLevel level)
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+            Exception exception = CreateDefaultException();
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(true)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message, exception))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Log(level, message, exception);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message, exception), Times.Once);
+        }
+        
+        [Test]
+        public void Log_GivenMessageAndExceptionWithNonLoggableLevel_DoesNotLogData([Values] LogLevel level)
+        {
+            // Arrange - Data
+            const string message = DefaultMessage;
+            Exception exception = CreateDefaultException();
+
+            // Arrange - Expectations
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.IsLoggable(level))
+                .Returns(false)
+                .Verifiable();
+            Mock.Get(MockLogger)
+                .Setup(mock => mock.Log(level, message, exception))
+                .Verifiable();
+
+            // Act
+            ClassUnderTest.Log(level, message, exception);
+
+            // Verify
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.IsLoggable(level), Times.AtLeastOnce);
+            Mock.Get(MockLogger)
+                .Verify(mock => mock.Log(level, message, exception), Times.Never);
+        }
+    }
+}
