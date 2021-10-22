c757d030ea434ac8d7f0f1d1b032c13e81259a60
Bishop S Johnson <bishop@enjin.io>
2021-10-21 19:53:11 -0700

    Added `HttpLogLevel` enum class

---
+++ EnjinCSharpSDK/Http/HttpLogLevel.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+
+namespace Enjin.SDK.Http
+{
+    /// <summary>
+    /// Enum for different logging levels of HTTP traffic.
+    /// </summary>
+    [PublicAPI]
+    public enum HttpLogLevel
+    {
+        /// <summary>
+        /// No logging.
+        /// </summary>
+        NONE,
+
+        /// <summary>
+        /// Logs request and response lines.
+        /// </summary>
+        BASIC,
+
+        /// <summary>
+        /// Logs request and response lines as well as their respective headers.
+        /// </summary>
+        HEADERS,
+
+        /// <summary>
+        /// Logs request and response lines as well as their respective headers and bodies if present.
+        /// </summary>
+        BODY,
+    }
+}
