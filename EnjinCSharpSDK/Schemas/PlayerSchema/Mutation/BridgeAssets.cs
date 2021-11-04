ca56c8331b462140a823cbeb7598c32bcf9824d0
Bishop Johnson <BishopJohnson@users.noreply.github.com>
2021-11-04 16:25:06 -0700

    [SDK 92] JumpNet Bridge mutations (#46)
    * Added template for `BridgeAsset`, `BridgeAssets`, and `BridgeClaimAsset` mutation
    * Added request classes for `BridgeAsset`, `BridgeAssets`, and `BridgeClaimAsset` mutation
    * Updated `IProjectSchema` and `IPlayerSchema` for new Bridge mutations

---
+++ EnjinCSharpSDK/Schemas/PlayerSchema/Mutation/BridgeAssets.cs
@@ -0,0 +1,59 @@
+﻿/* Copyright 2021 Enjin Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Enjin.SDK.Graphql;
+using Enjin.SDK.Shared;
+using JetBrains.Annotations;
+
+namespace Enjin.SDK.PlayerSchema
+{
+    /// <summary>
+    /// Request for bridging multiple indices of an NFT in a single transaction.
+    /// </summary>
+    /// <seealso cref="BridgeAsset"/>
+    /// <seealso cref="IPlayerSchema"/>
+    [PublicAPI]
+    public class BridgeAssets : GraphqlRequest<BridgeAssets>, ITransactionFragmentArguments<BridgeAssets>
+    {
+        /// <summary>
+        /// Sole constructor.
+        /// </summary>
+        public BridgeAssets() : base("enjin.sdk.player.BridgeAssets")
+        {
+        }
+
+        /// <summary>
+        /// Sets the ID of the asset to bridge.
+        /// </summary>
+        /// <param name="assetId">The asset ID.</param>
+        /// <returns>This request for chaining.</returns>
+        public BridgeAssets AssetId(string? assetId)
+        {
+            SetVariable("assetId", assetId);
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the indices to bridge.
+        /// </summary>
+        /// <param name="assetIndices">The asset indices.</param>
+        /// <returns>This request for chaining.</returns>
+        public BridgeAssets AssetIndices(params string[]? assetIndices)
+        {
+            SetVariable("assetIndices", assetIndices);
+            return this;
+        }
+    }
+}
---
+++ EnjinCSharpSDK/Schemas/PlayerSchema/Mutation/BridgeClaimAsset.cs
@@ -0,0 +1,47 @@
+﻿/* Copyright 2021 Enjin Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Enjin.SDK.Graphql;
+using Enjin.SDK.Shared;
+using JetBrains.Annotations;
+
+namespace Enjin.SDK.PlayerSchema
+{
+    /// <summary>
+    /// Request for claiming assets on the bridge.
+    /// </summary>
+    /// <seealso cref="IPlayerSchema"/>
+    [PublicAPI]
+    public class BridgeClaimAsset : GraphqlRequest<BridgeClaimAsset>, ITransactionFragmentArguments<BridgeClaimAsset>
+    {
+        /// <summary>
+        /// Sole constructor.
+        /// </summary>
+        public BridgeClaimAsset() : base("enjin.sdk.player.BridgeClaimAsset")
+        {
+        }
+
+        /// <summary>
+        /// Sets which assets to filter for by their ID.
+        /// </summary>
+        /// <param name="assetId">The asset IDs to filter for.</param>
+        /// <returns>This request for chaining.</returns>
+        public BridgeClaimAsset AssetId(string? assetId)
+        {
+            SetVariable("assetId", assetId);
+            return this;
+        }
+    }
+}
