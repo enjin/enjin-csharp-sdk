29a32245332336a0a2ef632694affbeac1d4387b
Bishop Johnson <BishopJohnson@users.noreply.github.com>
2022-03-27 22:21:56 -0700

    Removed and replaced `JObject` types in public API (#63)
    * Replaced `JObject` types in model classes with `IDictionary<string, object>`
    
    * Changed `CreateRequestBody()` method in `BaseSchema` from protected to private

--- EnjinCSharpSDK/Models/TransactionEvent.cs
+++ EnjinCSharpSDK/Models/TransactionEvent.cs
@@ -16,7 +16,6 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Enjin.SDK.Models
 {
@@ -32,35 +31,35 @@ namespace Enjin.SDK.Models
         /// <value>The name.</value>
         [JsonProperty("name")]
         public string? Name { get; private set; }
-        
+
         /// <summary>
         /// Represents the parameters.
         /// </summary>
         /// <value>The parameters.</value>
         [JsonProperty("inputs")]
-        public List<JObject>? Inputs { get; private set; }
-        
+        public List<IDictionary<string, object>>? Inputs { get; private set; }
+
         /// <summary>
         /// Represents the non-indexed parameters.
         /// </summary>
         /// <value>The non-indexed parameters.</value>
         [JsonProperty("nonIndexedInputs")]
-        public List<JObject>? NonIndexedInputs { get; private set; }
-        
+        public List<IDictionary<string, object>>? NonIndexedInputs { get; private set; }
+
         /// <summary>
         /// Represents the indexed parameters.
         /// </summary>
         /// <value>The indexed parameters.</value>
         [JsonProperty("indexedInputs")]
-        public List<JObject>? IndexedInputs { get; private set; }
-        
+        public List<IDictionary<string, object>>? IndexedInputs { get; private set; }
+
         /// <summary>
         /// Represents the event signature, or <c>null</c> if anonymous.
         /// </summary>
         /// <value>The signature.</value>
         [JsonProperty("signature")]
         public string? Signature { get; private set; }
-        
+
         /// <summary>
         /// Represents the encoded signature.
         /// </summary>
